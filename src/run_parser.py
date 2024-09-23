8ef1e329beec015ed1ed64acf5c4e6fa4e637576
daniel_droder <daniel.droder@oracle.com>
2024-09-23 11:32:11 -0400

    cleaning

--- src/run_parser.py
+++ src/run_parser.py
@@ -7,7 +7,7 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.abspath(os.path.join(current_dir, ".."))
 sys.path.append(parent_dir)
 
-from wowclp import Parser
+from parser import Parser
 
 if __name__ == "__main__":
     psr = Parser()
